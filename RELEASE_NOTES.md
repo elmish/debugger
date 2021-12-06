e04408e206c9ea37fab96fa4b225e685e0c75d17
Maxime Mangel <mangel.maxime@protonmail.com>
2021-12-06 13:49:16 +0100

    Move documentation to Nacara

--- RELEASE_NOTES.md
+++ RELEASE_NOTES.md
@@ -1,117 +1,117 @@
-### 4.0.0-beta-1
+## 4.0.0-beta-1
 
 * Elmish 4.0 support
 
-### 3.3.0
+## 3.3.0
 
 * Upgrade Thoth.Json to version 6
 * Keep FSharp.Core requirements lowered to 4.7.2
 
-### 3.2.0
+## 3.2.0
 
 * Release stable version
 
-### 3.2.0-beta-1
+## 3.2.0-beta-1
 
 * Upgrade Thoth.Json to version 4
 
-### 3.1.0
+## 3.1.0
 
 * Fix #38: Upgrade Thoth.Json to latest version
 
-### 4.0.0-alpha-1
+## 4.0.0-alpha-1
 
 * Elmish 3.0 release
 
-### 3.0.3
+## 3.0.3
 
 * Fix #33: getActionType via remotedev
 * Better display of nested messages
 
-### 3.0.2
+## 3.0.2
 
 * Fix #30: Add Thoth extra coders
 
-### 3.0.1
+## 3.0.1
 
 * Fix #28: Converting circular structure to JSON
 
-### 3.0.0
+## 3.0.0
 
 * Elmish 3.0 release
 
-### 3.0.0-beta-3
+## 3.0.0-beta-3
 
 * Elmish 3.0 opaque `Program` support
 
-### 3.0.0-beta-2
+## 3.0.0-beta-2
 
 * Elmish 3.0 compat
 
-### 2.0.2
+## 2.0.2
 
 * Thoth serialization by Alfonso
 
-### 2.0.0
+## 2.0.0
 
 * Stable release for Fable2
 
-### 2.0.0-beta-5
+## 2.0.0-beta-5
 
 * inlining for Fable2 reflection to work
 
-### 2.0.0-beta-4
+## 2.0.0-beta-4
 
 * Re-releasing v1 for Fable2
 
-### 1.0.2
+## 1.0.2
 
 * backporting the build
 
-### 1.0.1
+## 1.0.1
 
 * report errors fix by @zaaack
 
-### 1.0.0
+## 1.0.0
 
 * debounce by Alfonso
 
-### 1.0.0-beta-1
+## 1.0.0-beta-1
 
 * dotnet 2.0 SDK build
 
-### 0.9.0
+## 0.9.0
 
 * Fable 1.1.x build
 
-### 0.9.0-beta-6
+## 0.9.0-beta-6
 
 * Using FSharp.Reflection, all the glory goes to Jonathan Ohlrich
 
-### 0.9.0-beta-1
+## 0.9.0-beta-1
 
 * Targeting Fable 1.x
 
-### 0.1.2
+## 0.1.2
 
 * Safely continue if the monitor is not available
 
-### 0.1.1
+## 0.1.1
 
 * Adding basic import support
 
-### 0.1.0
+## 0.1.0
 
 * Releasing the debugger support
 
-### 0.1.0-alpha.5
+## 0.1.0-alpha.5
 
 * Everyting that Fable can `inflate` should work when time-traveling
 
-### 0.1.0-alpha.2
+## 0.1.0-alpha.2
 
 * Simple state works
 
-### 0.1.0-alpha.1
+## 0.1.0-alpha.1
 
 * Initial debugger release
--- docs/index.fsx
+++ docs/index.fsx
@@ -1,11 +1,25 @@
+(**
+---
+layout: standard
+title:
+toc: false
+---
+**)
+
 (*** hide ***)
-#I "../../src/bin/Debug/netstandard1.6"
-#r "Fable.Elmish.dll"
-#r "Fable.Elmish.Debugger.dll"
+
+#load "./prelude.fsx"
+
+let view _ _ = failwith "view not implemented"
+let update _ _ = failwith "update not implemented"
+let init _ = failwith "init not implemented"
+
 open Elmish
 
-(** Remote DevTools debugger
-======================
+(**
+
+## Remote DevTools debugger
+
 Elmish applications can benefit from sophisiticated time-travelling debuger with deep state and message inspection capabilities and import/export functionality.
 Wether you target browser, native or any other platform, as long as you can connect to a monitor you can start collecting and visualizing the events.
 For SPAs running in a browser, it's as easy as installing a plugin.
@@ -15,17 +29,17 @@ For SPAs running in a browser, it's as easy as installing a plugin.
 
 ### Installation
 Add Remote DevTools client package as a devDependency:
-```shell
+```sh
 yarn add remotedev@^0.2.4 -D
 ```
 
-and add Fable package with paket:
+and add Fable package:
 
-```shell
-paket add nuget Fable.Elmish.Debugger
+```sh
+dotnet add package Fable.Elmish.Debugger
 ```
 
-Follow the monitor installation instructions at [Remotedev tools](https://github.com/zalmoxisus/remotedev) site. 
+Follow the monitor installation instructions at [Remotedev tools](https://github.com/zalmoxisus/remotedev) site.
 
 Among all the monitoring apps mentioned there, for local web debugging in Chrome we recommend using [Redux DevTools Chrome extension](https://chrome.google.com/webstore/detail/redux-devtools/lmhkpmbekcpmknklioeibfkpmmfibljd) as the fastest and the most seamless monitoring option.
 
@@ -52,7 +66,7 @@ or in case of a remote debugger:
 open Elmish.Debug
 
 Program.mkProgram init update view
-|> Program.withDebuggerAt (Remote("localhost",8000)) // connect to a server running on localhost:8000
+|> Program.withDebuggerAt (Debugger.Remote("localhost",8000)) // connect to a server running on localhost:8000
 |> Program.run
 
 
@@ -65,10 +79,10 @@ or, using a custom connection:
 
 open Elmish.Debug
 
-let connection = Debugger.connect (Remote("localhost",8080)) // obtain the connection, for example if sending some information directly
+let connection = Debugger.connect (Debugger.Remote("localhost",8080)) // obtain the connection, for example if sending some information directly
 
 Program.mkProgram init update view
-|> Program.withDebuggerUsing connection
+|> Program.withDebuggerConnection connection
 |> Program.run
 
 
