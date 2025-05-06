73b84aa30b07468ec1cc3e1463da264610caf811
Marduk Bolaños <mardukbp@mac.com>
2025-05-06 15:13:28 +0200

    Update docs (#75)
    * Updated the installation instructions. Local debugging needs jsan, remote debugging remotedev.
    
    * Fixed typos in the documentation

--- docs/index.fsx
+++ docs/index.fsx
@@ -20,7 +20,7 @@ open Elmish
 
 ## Remote DevTools debugger
 
-Elmish applications can benefit from sophisiticated time-travelling debuger with deep state and message inspection capabilities and import/export functionality.
+Elmish applications can benefit from sophisticated time-travelling debugger with deep state and message inspection capabilities and import/export functionality.
 Wether you target browser, native or any other platform, as long as you can connect to a monitor you can start collecting and visualizing the events.
 For SPAs running in a browser, it's as easy as installing a plugin.
 
@@ -28,12 +28,18 @@ For SPAs running in a browser, it's as easy as installing a plugin.
 
 
 ### Installation
-Add Remote DevTools client package as a devDependency:
+For local debugging using the Redux DevTools browser extension add the following devDependency:
+
+```sh
+yarn add jsan@^3.1.14 -D
+```
+
+For connecting to a remote debugger add the Remote DevTools client package as a devDependency:
 ```sh
 yarn add remotedev@^0.2.4 -D
 ```
 
-and add Fable package:
+Then add Fable package:
 
 ```sh
 dotnet add package Fable.Elmish.Debugger
